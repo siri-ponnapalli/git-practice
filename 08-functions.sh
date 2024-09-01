d65a3866cb9725a11f1ff000a12120eff4c1422f
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-09-01 23:57:20 +0530

    output.sh

---
+++ 08-functions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+USERID=$(id -u)
+echo "USER ID is: $USERID"
+
+
+
+VALIDATE(){
+    echo "exit status: $1"
+
+}
+if "[ $USERID -ne 0]"
+then
+    echo "Please run this script with root priveleges"
+    exit 1
+
+fi
+
+dnf install git -y
+dnf list installed git
+VALIDATE $?
+
+
+# if [ $? -ne 0]
+# then 
+#     echo "Git is not installed,going to install it.."
+#     dnf install git -y
+#     if [ $? -ne o ]
+#     then 
+#        echo "Git installation is not sucess...check it"
+#        exit 1
+# else 
+#    echo "Git installation is sucess"
+# fi
+
+# else 
+#    echo "Git is already installed/ noting to do.."
+# fi
+
+

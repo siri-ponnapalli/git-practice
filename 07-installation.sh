b99d12d52ea7df612ac3441bf72566a4904d150b
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-09-01 21:44:15 +0530

    output.sh

--- 07-installation.sh
+++ 07-installation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 USERID=$(id -u)
 echo "USER ID is: $USERID"
-if [ $USERID -ne 0]
+if "[ $USERID -ne 0]"
 then
     echo "Please run this script with root priveleges"
     exit 1
-    
+
 fi
 
 dnf install git -y

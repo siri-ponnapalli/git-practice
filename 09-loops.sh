e81d5d3d0aba2315f4cae4bf41d6ba03a4fa7fee
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-09-02 01:11:06 +0530

    output.sh

---
+++ 09-loops.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+USERID=$(id -u)
+#echo "USER ID is: $USERID"
+
+
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo "$2is...FAILED"
+       exit 1
+    else
+       echo "$2 is...SUCCESS"
+       exit
+       fi
+}
+
+CHECK_ROOT() {
+    if [ $USERID -ne 0 ]
+    then
+    echo "Please run this script with root priveleges"
+    exit 1
+    fi
+}
+
+CHECK_ROOT
+
+for packages in $@
+do
+
+   echo $packages
+
+done
+
+
+
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed,going to install it.."
+    dnf install git -y
+    if [ $? -ne o ]
+    then 
+       echo "Git installation is not sucess...check it"
+       exit 1
+    else 
+       echo "Git installation is sucess"
+    fi
+
+else 
+   echo "Git is already installed/ noting to do.."
+ fi
